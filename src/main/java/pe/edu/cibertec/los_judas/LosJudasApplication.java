d3837d28ce9d664776d0641ebfed787ba8d77bf7
Natha23XO <hugocastroleon2301@gmail.com>
2024-10-25 21:32:36 -0500

    Subiendo a repo

---
+++ src/main/java/pe/edu/cibertec/los_judas/LosJudasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.los_judas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosJudasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosJudasApplication.class, args);
+	}
+
+}
